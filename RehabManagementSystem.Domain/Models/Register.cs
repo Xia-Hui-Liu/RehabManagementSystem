1a06bd635d04ec8d669a22c8c0efcb8157fa48d7
xiahuiliu <coffeeliuxiahui@gmail.com>
2024-09-25 14:18:12 +0200

    added login and register models

---
+++ RehabManagementSystem.Domain/Models/Register.cs
@@ -0,0 +1,9 @@
+
+namespace RehabManagementSystem.Domain;
+
+public class Register
+{
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+}
